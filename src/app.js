678648c4b1fceee8b69a943191ddebe64dd457ea
Daira <dasamgir@hotmail.com>
2019-06-07 16:14:44 -0500

    Agregar funciones

--- src/app.js
+++ src/app.js
@@ -6,10 +6,14 @@ function start(){
             totalTime: {value: "0:00", DOMElement: document.querySelector(".totalTime")},
             currentTime: {value: "0:00", DOMElement: document.querySelector(".currentTime")},
             progressBar: {value: "0:00", DOMElement: document.querySelector(".progressBar")},
+            songName: {value: "....", DOMElement: document.querySelector(".songName")},
+            artistName: {value: "....", DOMElement: document.querySelector(".artistName")},
         },
         buttons:{
             playPause: document.querySelector(".play"),
-            volume: document.querySelector(".volume")
+            volume: document.querySelector(".volume"),
+            next: document.querySelector(".next"),
+            back: document.querySelector(".previous")
         }
     });
 }
